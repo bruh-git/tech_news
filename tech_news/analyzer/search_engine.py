12666d6613ed2ddba6e5501b10626686d3e35629
bruh-git <brunac.cardososilva@gmail.com>
2022-11-10 12:07:25 -0300

    feat: req 09

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -26,4 +26,7 @@ def search_by_tag(tag):
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    news_by_category = search_news(
+        {"category": {"$regex": category, "$options": "i"}}
+    )
+    return [(new["title"], new["url"]) for new in news_by_category]
